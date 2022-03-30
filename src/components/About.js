040536b0496857d038f14c4103f52d5c8cba6363
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-03-30 13:05:49 -0700

    added resume to about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,19 @@
 export default function About() {
   return (
     <>
-      <h1>About Page</h1>
+      <iframe
+        src={process.env.PUBLIC_URL + '/docs/andrewSchroepferResume10.2021.docx-1.pdf#view=Fit&zoom=70'}
+        title='Resume'
+        scrolling="no"
+        style={{
+          border: "none",
+          marginTop: "64px",
+          boxShadow: "0px 16px 16px rgba(0, 0, 0, 0.8)",
+          opacity: "0.9",
+          height: "800px",
+          width: "600px",
+        }}
+      ></iframe>
     </>
   );
 }
