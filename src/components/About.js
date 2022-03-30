1d03ab491fe3a4c52c4406d73cd062b27fd98196
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-03-30 13:15:25 -0700

    trying to fix git pages compatability

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 export default function About() {
   return (
-    <>
       <iframe
         src={process.env.PUBLIC_URL + '/docs/andrewSchroepferResume10.2021.docx-1.pdf#view=Fit&zoom=70'}
         title='Resume'
@@ -9,11 +8,9 @@ export default function About() {
           border: "none",
           marginTop: "64px",
           boxShadow: "0px 16px 16px rgba(0, 0, 0, 0.8)",
-          opacity: "0.9",
           height: "800px",
           width: "600px",
         }}
       ></iframe>
-    </>
   );
 }
