c986a3575d18de5f486331584c31b25989af3889
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-04-07 17:06:34 -0700

    adjusted tetris app to fit landscape phones

--- src/components/portfolio/projects/tetris/TPStyles.js
+++ src/components/portfolio/projects/tetris/TPStyles.js
@@ -6,7 +6,7 @@ export const TetrisPortfolioBox = styled.div`
 export const TetrisFrame = styled.iframe`
   width: 480px;
   max-width: 100%;
-  min-height: 480px;
+  max-height: calc(100vh - 64px);
   height: calc(667px - calc(50vw - 250px));
   border: 0;
   border-radius: 4px;
